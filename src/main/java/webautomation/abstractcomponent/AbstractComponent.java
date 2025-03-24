53a5a28c4e3f819627713b8e777da530e0406d9d
Novel Yourdhan Rehandhika <novel.yourdhan.r@gmail.com>
2025-03-24 17:13:18 +0700

    test jenkins

--- src/main/java/webautomation/abstractcomponent/AbstractComponent.java
+++ src/main/java/webautomation/abstractcomponent/AbstractComponent.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,8 +15,16 @@ public class AbstractComponent {
     this.driver = driver;
   }
 
+  // by locator
   public void visibilityOfElementLocated(By locator) {
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
   }
+
+  // web element
+  public void visibilityOfElement(WebElement locator) {
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    wait.until(ExpectedConditions.visibilityOf(locator));
+  }
+
 }
